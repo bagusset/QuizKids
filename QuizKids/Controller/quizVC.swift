372fb2031120113a7acb7792f0c99a0591f37901
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-11-08 21:52:53 +0700

    add file categoryVC & quizVC !

---
+++ QuizKids/Controller/quizVC.swift
@@ -0,0 +1,29 @@
+//
+//  quizVC.swift
+//  QuizKids
+//
+//  Created by Bagus setiawan on 08/11/20.
+//
+
+import UIKit
+
+class quizVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
