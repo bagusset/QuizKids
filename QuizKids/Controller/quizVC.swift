a44e611697e3bbd31f965cb0705726330656f343
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-11-09 08:26:53 +0700

    add IIBOutlet !

--- QuizKids/Controller/quizVC.swift
+++ QuizKids/Controller/quizVC.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class quizVC: UIViewController {
 
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionImage: UIImageView!
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
