68d4b601baa250f529302cec8c0f59b2858c4c6b
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-11-09 20:29:39 +0700

    fix DataLoader !

--- QuizKids/Util/DataLoader.swift
+++ QuizKids/Util/DataLoader.swift
@@ -7,11 +7,38 @@
 
 import Foundation
 
-//class DataLoader {
-//    private static let fileExtension = "json"
-//    
-//    private static let categoryFileName = "category"
-//    private static let questionFileName = "question"
-//    
-//    static func getCategorys() -> []
-//}
+class DataLoader {
+    private static let fileExtension = "json"
+    
+    private static let categoryFileName = "category"
+    private static let questionFileName = "question"
+    
+    static func getCategories() -> [Category] {
+        var categories = [Category]()
+        if let path = Bundle.main.url(forResource: categoryFileName, withExtension: fileExtension){
+            do{
+                let data = try Data(contentsOf: path)
+                let decoder = JSONDecoder()
+                categories = try decoder.decode([Category].self, from: data)
+            } catch{
+                print(error)
+            }
+        }
+        return categories
+    }
+    
+    static func getQuestion() -> [Question]{
+        var questions = [Question]()
+        
+        if let path = Bundle.main.url(forResource: questionFileName, withExtension: fileExtension) {
+            do{
+                let data = try Data(contentsOf: path)
+                let decoder = JSONDecoder()
+                questions = try decoder.decode([Question].self, from: data)
+            } catch {
+                print(error)
+            }
+        }
+        return questions
+    }
+}
